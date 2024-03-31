19a9c2b8a37f021fa2f891a61adedb7a2bd21733
SireJeff <61094553+Mshiri@users.noreply.github.com>
2024-03-31 22:48:26 +0330

    new file:   slave-docker/prevsla.py 	modified:   slave-docker/slave.py

--- slave-docker/prevsla.py
+++ slave-docker/prevsla.py
@@ -4,9 +4,6 @@ from telebot import types
 
 # Your Telegram bot token
 BOT_TOKEN = "6899480458:AAEDERCh7X4RefFxaIvNpW4PFQkLFqVex_Y"
-CHANNEL_ID ="@vpn_pvc"
-# Password for authentication
-PASSWORD = "Myhorseisblack69"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -23,39 +20,36 @@ def make_api_call(url):
     except Exception as e:
         print("Error occurred:", e)
         return []
+CHANNEL_ID = "@your_channel_name_here"
 
-def send_strings_as_messages(ips, chat_id):
+def send_strings_as_messages(ips, channel_id):
     num_proxies = len(ips)
-    for i in range(0, num_proxies, 7):  # Step by 5 to group proxies into sets of 5
+    for i in range(0, num_proxies, 5):  # Step by 5 to group proxies into sets of 5
         message = ""
         keyboard = types.InlineKeyboardMarkup()
-        for j, ip_with_port in enumerate(ips[i:i+7], start=1):
+        for j, ip_with_port in enumerate(ips[i:i+5], start=1):
             # Extract IP address without port
             ip = ip_with_port.split(':')[0]
             label = f"Proxy{i + j}"
             message += f"{label} "
             url_button = types.InlineKeyboardButton(text=label, url=f"tg://proxy?server={ip}&port=33333&secret=eee601e300d8e3fb8bfcec8828f9cf08347777772e636c6f7564666c6172652e636f6d")
             keyboard.add(url_button)
-        bot.send_message(CHANNEL_ID, "@vpn_pvc free premium proxy and VPN \n share and react! \n جهت تداوم به اشتراک بگذارید", reply_markup=keyboard)
+        bot.send_message(channel_id, message, reply_markup=keyboard)
+
+
+
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Please enter the password to access the bot's functionalities:")
-    bot.register_next_step_handler(message,echo_all)
+    markup = types.ReplyKeyboardMarkup()
+    itembtn = types.KeyboardButton('Fetch and Send Data')
+    markup.row(itembtn)
+    bot.send_message(message.chat.id, "Press the button to fetch and send data:", reply_markup=markup)
 
+@bot.message_handler(func=lambda message: True)
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    if message.text == PASSWORD:
-        markup = types.ReplyKeyboardMarkup()
-        itembtn = types.KeyboardButton('Fetch and Send Data')
-        markup.row(itembtn)
-        bot.send_message(message.chat.id, "Press the button to fetch and send data:", reply_markup=markup)
-        bot.register_next_step_handler(message,handle_fetch)
-    else:
-        bot.send_message(message.chat.id, "Incorrect password. Please try again.")
-
-@bot.message_handler(func=lambda message: True)
-def handle_fetch(message):
     if message.text == 'Fetch and Send Data':
         # Fetch IPs from the first API endpoint
         ips1 = make_api_call("https://api.runonflux.io/apps/location/MEHD4")
@@ -64,7 +58,7 @@ def handle_fetch(message):
         if ips1 and ips2:
             all_ips = ips1 + ips2  # Concatenate IPs from both endpoints
             send_strings_as_messages(all_ips, message.chat.id)
-            #bot.send_message(message.chat.id, "Data fetched and sent successfully!")
+            bot.send_message(message.chat.id, "Data fetched and sent successfully!")
         else:
             bot.send_message(message.chat.id, "Failed to fetch data. Please try again later.")
 
